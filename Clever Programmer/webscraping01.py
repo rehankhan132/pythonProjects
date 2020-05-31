f70be46ddc6c7b0cd177f0c42c1f5b2757495147
rehankhan132 <rehankhan.mfdhiee@gmail.com>
2020-05-31 19:23:17 +0500

    BeautifulSoup Change 08

--- Clever Programmer/webscraping01.py
+++ Clever Programmer/webscraping01.py
@@ -8,9 +8,9 @@ week = soup.find(id='seven-day-forecast-body')
 # print(week)
 items = week.find_all(class_='tombstone-container')
 # print(items[0])
-print(items[0].find(class_='period-name').get_text())
-print(items[0].find(class_='short-desc').get_text())
-print(items[0].find(class_='temp').get_text())
+# print(items[0].find(class_='period-name').get_text())
+# print(items[0].find(class_='short-desc').get_text())
+# print(items[0].find(class_='temp').get_text())
 
 period_names = [item.find(class_='period-name').get_text() for item in items]
 short_descriptions = [item.find(class_='short-desc').get_text() for item in items]
@@ -23,5 +23,7 @@ temperatures = [item.find(class_='temp').get_text() for item in items]
 weather_stuff = pd.DataFrame ({
     'periad_names': period_names,
     'short-descriptions': short_descriptions,
-    'temeratures': temperatures
+    'tempratures': temperatures
 })
+print(weather_stuff)
+weather_stuff.to_csv('weather.csv')
