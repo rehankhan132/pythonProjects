cec495ea34873131cc4049d1b200a36d7c3b15b4
rehankhan132 <rehankhan.mfdhiee@gmail.com>
2020-05-31 14:55:38 +0500

    BeaurtifulSoup Code 01

---
+++ Clever Programmer/webscraping01.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get('https://forecast.weather.gov/MapClick.php?lat=34.099695000000054&lon=-118.33539999999999#.XsaFCGgzbIU')
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup)
