146c45e238c19fa2ae46329dbe17e1824b577a01
rehankhan132 <rehankhan.mfdhiee@gmail.com>
2020-05-31 15:12:45 +0500

    BeautifulSoup Change 02

--- Clever Programmer/webscraping01.py
+++ Clever Programmer/webscraping01.py
@@ -3,4 +3,5 @@ from bs4 import BeautifulSoup
 
 page = requests.get('https://forecast.weather.gov/MapClick.php?lat=34.099695000000054&lon=-118.33539999999999#.XsaFCGgzbIU')
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup)
+week = soup.find(id='seven-day-forecast-body')
+print(week)
