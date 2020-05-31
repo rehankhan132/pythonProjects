2267f889ab9a952c0e02f079b0b44423c756f874
rehankhan132 <rehankhan.mfdhiee@gmail.com>
2020-05-31 15:26:59 +0500

    BeautifulSoup Change 06

--- Clever Programmer/webscraping01.py
+++ Clever Programmer/webscraping01.py
@@ -4,4 +4,16 @@ from bs4 import BeautifulSoup
 page = requests.get('https://forecast.weather.gov/MapClick.php?lat=34.099695000000054&lon=-118.33539999999999#.XsaFCGgzbIU')
 soup = BeautifulSoup(page.content, 'html.parser')
 week = soup.find(id='seven-day-forecast-body')
-print(week)
+# print(week)
+items = week.find_all(class_='tombstone-container')
+# print(items[0])
+print(items[0].find(class_='period-name').get_text())
+print(items[0].find(class_='short-desc').get_text())
+print(items[0].find(class_='temp').get_text())
+
+period_names = [item.find(class_='period-name').get_text() for item in items]
+short_descriptions = [item.find(class_='short-desc').get_text() for item in items]
+temperatures = [item.find(class_='temp').get_text() for item in items]
+print(period_names)
+print(short_descriptions)
+print(temperatures)
