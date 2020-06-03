e7fb5feaa60423fe4bc9493730fbd388c0c2c4b1
rehankhan132 <rehankhan.mfdhiee@gmail.com>
2020-06-03 19:29:26 +0500

    ticTacToe Game 01

---
+++ Clever Programmer/ticTacToe.py
@@ -0,0 +1,22 @@
+board = ["-","-","-",
+        "-","-","-",
+        "-","-","-"]
+def display_board():
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+
+display_board()
+
+
+
+# Board
+# Display Game
+# Play Game
+# Handle Turn
+# Check Win
+#     Check Rows
+#     Check Columns
+#     Check Dioganals
+# Check Tie
+# Filp Player
