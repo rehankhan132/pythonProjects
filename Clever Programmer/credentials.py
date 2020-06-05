5ecb966043a20b0ac518b5deb86ecad44e85dfb1
rehankhan132 <rehankhan.mfdhiee@gmail.com>
2020-06-05 19:44:55 +0500

    tiwilio app 02

---
+++ Clever Programmer/credentials.py
@@ -0,0 +1,4 @@
+account_sid = 'ACd0a2b6a8e5148a79fb5a3e238d89377e'
+auth_token = '73e72d7c8d5e52414f7daca4b7f2c146'
+my_cell = '+923477022126'
+my_twilio = '+12058285448'
---
+++ Clever Programmer/tiwilio_example01.py
@@ -0,0 +1,13 @@
+# from twilio.rest import Client 
+ 
+# account_sid = 'ACd0a2b6a8e5148a79fb5a3e238d89377e' 
+# auth_token = '73e72d7c8d5e52414f7daca4b7f2c146' 
+# client = Client(account_sid, auth_token) 
+ 
+# message = client.messages.create( 
+#                               from_='+12058285448',  
+#                               body='Hello Rehan Khan',      
+#                               to='+923477022126' 
+#                           ) 
+ 
+# print(message.sid)
